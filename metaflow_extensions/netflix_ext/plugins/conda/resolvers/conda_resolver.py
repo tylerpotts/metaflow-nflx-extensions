a465bc1fd68d4ff5d94a8b4e58e167884fe0dd45
Tyler Potts <tyler.potts@dtn.com>
2023-10-13 13:21:11 -0500

    debug

--- metaflow_extensions/netflix_ext/plugins/conda/resolvers/conda_resolver.py
+++ metaflow_extensions/netflix_ext/plugins/conda/resolvers/conda_resolver.py
@@ -128,6 +128,8 @@ class CondaResolver(Resolver):
                     lnk["build_string"],
                     lnk["build_number"],
                 )
+                if k == '_libgcc_mutex_linux-64_conda_forge_0':
+                    import pdb; pdb.set_trace()
                 url, md5_hash = fetched_packages[k]
                 if not url.startswith(lnk["base_url"]):
                     raise CondaException(
