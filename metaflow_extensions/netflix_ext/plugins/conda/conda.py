734a10bca88c6b3b8a22e5d3ff38d06ed8699c60
Tyler Potts <tyler.potts@dtn.com>
2023-10-13 11:50:09 -0500

    a

--- metaflow_extensions/netflix_ext/plugins/conda/conda.py
+++ metaflow_extensions/netflix_ext/plugins/conda/conda.py
@@ -2036,6 +2036,7 @@ class Conda(object):
                             ret.setdefault(possible_env_id, []).append(entry.path)
         else:
             envs = self._info["envs"]  # type: List[str]
+            import pdb; pdb.set_trace()
             for env in envs:
                 with CondaLock(self.echo, self._env_lock_file(env)):
                     possible_env_id = _check_match(env)
